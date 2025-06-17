8f77d91d8da4eabdbfa25e430171239daaa7b288
Michael Polyntsov <Arno9148@gmail.com>
2025-06-17 15:24:19 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use teloxide::{prelude::*, utils::command::BotCommands};
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    log::info!("Starting command bot...");
+
+    let bot = Bot::from_env();
+
+    Command::repl(bot, answer).await;
+}
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "ping-pong.")]
+    Ping
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
+        Command::Ping  =>
+            bot.send_message(msg.chat.id, "Pong").await?,
+    };
+
+    Ok(())
+}
