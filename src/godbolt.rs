6e88d7c2e999eacd5db75ff0d9823e25d53f92fa
Michael Polyntsov <Arno9148@gmail.com>
2025-06-18 14:13:30 +0300

    Implement `compile` command

--- src/godbolt.rs
+++ src/godbolt.rs
@@ -1,21 +1,111 @@
 use reqwest::Error;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
 pub struct Language {
     pub id: String,
-    pub name: String
+    pub name: String,
+}
+
+#[derive(Debug, Serialize)]
+struct CompileRequest<'a> {
+    source: &'a str,
+    options: CompileOptions,
+}
+
+#[derive(Debug, Serialize)]
+struct CompileOptions {}
+
+#[derive(Debug, Deserialize)]
+struct AsmLine {
+    text: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct StderrLine {
+    text: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct CompileResponse {
+    asm: Vec<AsmLine>,
+    stderr: Vec<StderrLine>,
+    #[allow(dead_code)]
+    code: i32,
+}
+
+#[derive(Debug)]
+pub enum CompilationOutput {
+    Assembly(String),
+    Stderr(String),
+}
+
+const GODBOLT_URL: &str = "https://godbolt.org";
+
+fn route(path: &str) -> String {
+    format!("{GODBOLT_URL}/api/{path}")
+}
+
+/// # Description
+/// Compiles the given source code using the specified compiler ID.
+///
+/// # Arguments
+/// * `compiler_id` - The ID of the compiler (e.g., "g122" for GCC 12.2).
+/// * `code` - The source code to compile.
+///
+/// # Returns
+/// A `Result` which is:
+/// * `Ok(CompilationOutput)` on a successful API call. The enum will contain
+///   either the assembly or the compiler's stderr.
+/// * `Err(reqwest::Error)` if a network or deserialization error occurs.
+pub async fn compile(compiler_id: &str, code: &str) -> Result<CompilationOutput, Error> {
+    log::info!("Received '{code}' to compile with {compiler_id}.");
+
+    let request_url = route(&format!("compiler/{compiler_id}/compile"));
+
+    let request_body = CompileRequest {
+        source: code,
+        options: CompileOptions {},
+    };
+
+    let client = reqwest::Client::new();
+    let res = client
+        .post(request_url)
+        .header("Accept", "application/json")
+        .json(&request_body)
+        .send()
+        .await?;
+
+    let compile_res: CompileResponse = res.json().await?;
+
+    if !compile_res.stderr.is_empty() {
+        let error_output = compile_res
+            .stderr
+            .into_iter()
+            .map(|line| line.text)
+            .collect::<Vec<String>>()
+            .join("\n");
+        Ok(CompilationOutput::Stderr(error_output))
+    } else {
+        let assembly_output = compile_res
+            .asm
+            .into_iter()
+            .map(|line| line.text)
+            .collect::<Vec<String>>()
+            .join("\n");
+        Ok(CompilationOutput::Assembly(assembly_output))
+    }
 }
 
 pub async fn languages() -> Result<Vec<Language>, Error> {
-    let request_url = "https://godbolt.org/api/languages";
+    let request_url = route("languages");
     let client = reqwest::Client::new();
     let res = client
-        .get(format!("{request_url}"))
+        .get(request_url)
         .header("Accept", "application/json")
         .send()
         .await?;
-    let langs : Vec<Language> = res.json().await?;
+    let langs: Vec<Language> = res.json().await?;
 
     Ok(langs)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use teloxide::{prelude::*, types::ParseMode, utils::command::BotCommands, utils::markdown};
+use teloxide::{
+    prelude::*, types::MessageEntityKind, types::ParseMode, utils::command::BotCommands,
+    utils::markdown,
+};
 
 mod godbolt;
 
@@ -22,6 +25,8 @@ enum Command {
     Help,
     #[command(description = "pong.")]
     Ping,
+    #[command(description = "compile the code from the message.", aliases = ["c"])]
+    Compile,
     #[command(description = "list all supported languages.", aliases = ["ls"])]
     Languages,
 }
@@ -66,23 +71,95 @@ fn format_languages(langs: &[godbolt::Language]) -> String {
         .join("\n");
 
     let message = format!("{header}\n{separator}\n{formatted_langs}\n");
-    let safe_message = markdown::escape(&message);
-    return format!("```\n{safe_message}\n```");
+    return wrap_in_md(&message);
+}
+
+fn wrap_in_md(s: &str) -> String {
+    let safe_s = markdown::escape(s);
+    format!("```\n{safe_s}\n```")
+}
+
+async fn send_md(bot: &Bot, chat_id: ChatId, s: &str) -> ResponseResult<()> {
+    bot.send_message(chat_id, s)
+        .parse_mode(ParseMode::MarkdownV2)
+        .await?;
+    Ok(())
+}
+
+fn parse_compile_msg(msg: &Message) -> Result<(String, String), String> {
+    let parsed_entities = msg.parse_entities().unwrap_or_default();
+    let code_block = parsed_entities
+        .iter()
+        .filter_map(|entity| match entity.kind() {
+            MessageEntityKind::Pre { .. } | MessageEntityKind::Code => {
+                Some(entity.text().to_string())
+            }
+            _ => None,
+        })
+        .collect::<Vec<String>>();
+    let code_block_len = code_block.len();
+    if code_block_len != 1 {
+        let error_text =
+            format!("Invalid format. Expected exactly one code block, got {code_block_len}.");
+        return Err(error_text);
+    }
+    let code = code_block.first().unwrap();
+    let text = msg.text().unwrap_or_default();
+    let compile_full_command = text.replace(code, "");
+    let compiler_commands = compile_full_command
+        .split_whitespace()
+        .collect::<Vec<&str>>();
+    let compiler_id = match compiler_commands[..] {
+        [_command, compiler_id, ..] => Some(compiler_id),
+        [..] => None,
+    };
+
+    if let Some(id) = compiler_id {
+        return Ok((id.to_string(), code.to_string()));
+    } else {
+        return Err("Invalid format. Expected compile command.".to_string());
+    }
 }
 
 async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
     match cmd {
         Command::Help => {
             bot.send_message(msg.chat.id, Command::descriptions().to_string())
-                .await?
+                .await?;
         }
-        Command::Ping => bot.send_message(msg.chat.id, "Pong").await?,
+        Command::Ping => {
+            bot.send_message(msg.chat.id, "Pong").await?;
+        }
         Command::Languages => {
             let langs = godbolt::languages().await?;
             let message = format_languages(&langs);
-            bot.send_message(msg.chat.id, message)
-                .parse_mode(ParseMode::MarkdownV2)
-                .await?
+            send_md(&bot, msg.chat.id, &message).await?;
+        }
+        Command::Compile => {
+            let parse_result = parse_compile_msg(&msg);
+            match parse_result {
+                Ok((id, code)) => {
+                    let res = godbolt::compile(&id, &code).await?;
+                    match res {
+                        godbolt::CompilationOutput::Assembly(assembly) => {
+                            log::info!("Assembly: {assembly}");
+                            bot.send_message(msg.chat.id, wrap_in_md(&assembly))
+                                .parse_mode(ParseMode::MarkdownV2)
+                                .await?;
+                        }
+                        godbolt::CompilationOutput::Stderr(raw_err) => {
+                            log::info!("Error: {raw_err}");
+                            let err = strip_ansi_escapes::strip_str(&raw_err);
+                            bot.send_message(msg.chat.id, wrap_in_md(&err))
+                                .parse_mode(ParseMode::MarkdownV2)
+                                .await?;
+                        }
+                    }
+                }
+                Err(error) => {
+                    bot.send_message(msg.chat.id, error).await?;
+                }
+            }
         }
     };
 
